ad83559f5dc7a60f9de1db2b4516000738da0e9e
mateusb12 <mateus_b09@hotmail.com>
2020-12-07 01:24:17 -0300

    bootstrap installed

--- pyflask/app/controllers/default.py
+++ pyflask/app/controllers/default.py
@@ -1,6 +1,17 @@
 from app import app
+from flask import render_template
 
 
-@app.route("/")
-def index():
-    return "Hello World!"
+@app.route("/index/<user>")
+@app.route("/", defaults={'user': None})
+def index(user):
+    return render_template('index.html',
+                           user=user)
+
+# @app.route("/test", defaults={'name': None})
+# @app.route("/test/<name>")
+# def test(name=None):
+#     if name:
+#         return "Hello, {}".format(name)
+#     else:
+#         return "Hello!"
