ae8be78377c190ed6772866d2fefab5827d94eff
mateusb12 <mateus_b09@hotmail.com>
2020-12-26 13:14:41 -0300

    backup reasons, too many bugs

--- pyflask/app/models/forms.py
+++ pyflask/app/models/forms.py
@@ -37,14 +37,15 @@ class CalculatorForm(FlaskForm):
     class_choices = [i.capitalize() for i in jobname_list]
 
     headtop_choices = ['Ribbon']
-    headmid_choices = ['No headgear']
-    headlow_choices = ['No headgear']
+    headmid_choices = ['Ribbon']
+    headlow_choices = ['Ribbon']
     weapon_choices = ['No weapon']
-    shield_choices = ['No shield']
-    shoes_choices = ['No shoes']
-    robe_choices = ['No robe']
-    accessory_choices = ['No accessory']
-    armor_choices = ['No armor']
+    shield_choices = ['Guard']
+    shoes_choices = ['Boots']
+    robe_choices = ['Muffler']
+    accessory1_choices = ['Clip']
+    accessory2_choices = ['Ring']
+    armor_choices = ['Silk Robe']
 
     card_choices = ['Poring Card']
 
@@ -59,26 +60,43 @@ class CalculatorForm(FlaskForm):
     player_dex = SelectField('player_int', choices=list(range(1, 100)))
     player_luk = SelectField('player_int', choices=list(range(1, 100)))
 
-    headtop_item = SelectField('headtop_item', choices=headtop_choices)
-    headtop_refine = SelectField('headtop_refine', choices=list(range(1, 11)))
-    headtop_card_list = SelectField('headtop_card_list', choices=card_choices)
+    headtop_item = SelectField(choices=headtop_choices)
+    headtop_refine = SelectField(choices=list(range(1, 11)))
+    headtop_card_list = SelectField(choices=card_choices)
 
-    headmid_item = SelectField('headmid_item', choices=headmid_choices)
-    headmid_refine = SelectField('headmid_refine', choices=list(range(1, 11)))
-    headmid_card_list = SelectField('headmid_card_list', choices=card_choices)
+    headmid_item = SelectField(choices=headmid_choices)
+    headmid_refine = SelectField(choices=list(range(1, 11)))
+    headmid_card_list = SelectField(choices=card_choices)
 
-    headlow_item = SelectField('headlow_item', choices=headlow_choices)
-    headlow_refine = SelectField('headlow_refine', choices=list(range(1, 11)))
-    headlow_card_list = SelectField('headlow_card_list', choices=card_choices)
+    headlow_item = SelectField(choices=headlow_choices)
+    headlow_refine = SelectField(choices=list(range(1, 11)))
+    headlow_card_list = SelectField(choices=card_choices)
 
+    weapon_item = SelectField(choices=weapon_choices)
 
-    weapon_item = SelectField('headtop_item', choices=weapon_choices)
-    shield_item = SelectField('headtop_item', choices=shield_choices)
-    shoes_item = SelectField('headtop_item', choices=shoes_choices)
-    armor_item = SelectField('armor_item', choices=armor_choices)
-    robe_item = SelectField('armor_item', choices=robe_choices)
-    accessory1_item = SelectField('armor_item', choices=accessory_choices)
-    accessory2_item = SelectField('armor_item', choices=accessory_choices)
+    shield_item = SelectField(choices=shield_choices)
+    shield_refine = SelectField(choices=list(range(1, 11)))
+    shield_card_list = SelectField(choices=card_choices)
+
+    shoes_item = SelectField(choices=shoes_choices)
+    shoes_refine = SelectField(choices=list(range(1, 11)))
+    shoes_card_list = SelectField(choices=card_choices)
+
+    armor_item = SelectField(choices=armor_choices)
+    armor_refine = SelectField(choices=list(range(1, 11)))
+    armor_card_list = SelectField(choices=card_choices)
+
+    robe_item = SelectField(choices=robe_choices)
+    robe_refine = SelectField(choices=list(range(1, 11)))
+    robe_card_list = SelectField(choices=card_choices)
+
+    accessory1_item = SelectField(choices=accessory1_choices)
+    accessory1_refine = SelectField(choices=list(range(1, 11)))
+    accessory1_card_list = SelectField(choices=card_choices)
+
+    accessory2_item = SelectField(choices=accessory2_choices)
+    accessory2_refine = SelectField(choices=list(range(1, 11)))
+    accessory2_card_list = SelectField(choices=card_choices)
 
 
 def calc_dynamic_select(input_form):
@@ -100,11 +118,18 @@ def calc_dynamic_select(input_form):
     form.weapon_item.choices = sorted(equipable_list['weapon'])
     form.shield_item.choices = sorted(equipable_list['shield'])
     form.shoes_item.choices = sorted(equipable_list['shoes'])
-    form.armor_item.choices = sorted(equipable_list['shoes'])
-    form.robe_item.choices = sorted(equipable_list['shoes'])
+    form.armor_item.choices = sorted(equipable_list['armor'])
+    form.robe_item.choices = sorted(equipable_list['robe'])
     form.accessory1_item.choices = sorted(equipable_list['accessory'])
     form.accessory2_item.choices = sorted(equipable_list['accessory'])
 
-    form.headtop_card_list.choices = get_cardlist_by_name(form.headtop_item.data)
+    form.headtop_card_list.choices = sorted(get_cardlist_by_name(form.headtop_item.data))
     form.headmid_card_list.choices = get_cardlist_by_name(form.headmid_item.data)
     form.headlow_card_list.choices = get_cardlist_by_name(form.headlow_item.data)
+    form.shield_card_list.choices = get_cardlist_by_name(form.shield_item.data)
+    form.shoes_card_list.choices = get_cardlist_by_name(form.shoes_item.data)
+    form.armor_card_list.choices = get_cardlist_by_name(form.armor_item.data)
+    form.robe_card_list.choices = get_cardlist_by_name(form.robe_item.data)
+    form.accessory1_card_list.choices = get_cardlist_by_name(form.accessory1_item.data)
+    form.accessory2_card_list.choices = get_cardlist_by_name(form.accessory2_item.data)
+
